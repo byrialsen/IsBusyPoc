7a6d6d5ac037755d2b4116947a1530e7d94ba438
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2018-09-14 10:29:42 +0200

    Crated

--- IsBusyPoc/MainPage.xaml.cs
+++ IsBusyPoc/MainPage.xaml.cs
@@ -22,9 +22,59 @@ namespace IsBusyPoc
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+
+        public bool IsEnabled
+        {
+            get { return (bool)GetValue(IsEnabledProperty); }
+            set { SetValue(IsEnabledProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.Register("IsEnabled", typeof(bool), typeof(MainPage), new PropertyMetadata(null));
+
+
+
+        public bool IsLoadingToolkit
+        {
+            get { return (bool)GetValue(IsLoadingToolkitProperty); }
+            set { SetValue(IsLoadingToolkitProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLoadingToolkit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLoadingToolkitProperty =
+            DependencyProperty.Register("IsLoadingToolkit", typeof(bool), typeof(MainPage), new PropertyMetadata(null));
+
+        public bool IsLoadingTelerik
+        {
+            get { return (bool)GetValue(IsLoadingTelerikProperty); }
+            set { SetValue(IsLoadingTelerikProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLoadingTelerik.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLoadingTelerikProperty =
+            DependencyProperty.Register("IsLoadingTelerik", typeof(bool), typeof(MainPage), new PropertyMetadata(null));
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            (this as FrameworkElement).DataContext = this;
+
+            IsEnabled = true;
+        }
+
+        private void OnIsLoadingClick(object sender, RoutedEventArgs e)
+        {
+            IsLoadingToolkit = !IsLoadingToolkit;
+            IsEnabled = !IsLoadingToolkit;
+        }
+
+        private void OnIsLoading2Click(object sender, RoutedEventArgs e)
+        {
+            IsLoadingTelerik = !IsLoadingTelerik;
+            IsEnabled = !IsLoadingTelerik;
         }
     }
 }
